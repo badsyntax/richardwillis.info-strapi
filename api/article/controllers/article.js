31de689405d144144dcf0f37508e749407093207
Richard Willis <willis.rh@gmail.com>
2021-09-11 23:07:11 +0100

    Add article comments

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -1,16 +1,26 @@
 const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
-  /**
-   * Retrieve a record.
-   *
-   * @return {Object}
-   */
-
   async findOne(ctx) {
     const { slug } = ctx.params;
 
     const entity = await strapi.services.article.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+
+  async comment(ctx) {
+    ctx.request.body.article = ctx.params.id;
+    const entity = await strapi.services.comment.create({
+      published_at: null,
+      ...ctx.request.body,
+    });
+    await strapi.plugins['email'].services.email.send({
+      to: 'willis.rh@gmail.com',
+      from: 'strapi@richardwillis.info',
+      subject: 'New comment posted',
+      text: 'Hello world!',
+      html: 'Hello world!',
+    });
+    return sanitizeEntity(entity, { model: strapi.models.comment });
   },
 };
