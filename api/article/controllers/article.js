f1c1b949b1e1569d3c729668ea634d121dfd3b71
Richard Willis <willis.rh@gmail.com>
2021-09-12 08:18:00 +0100

    Update comments endpoints and improve email notification

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -14,12 +14,15 @@ module.exports = {
       published_at: null,
       ...ctx.request.body,
     });
+    const article = await strapi.services.article.findOne({
+      id: ctx.params.id,
+    });
     await strapi.plugins['email'].services.email.send({
       to: 'willis.rh@gmail.com',
       from: 'strapi@richardwillis.info',
-      subject: 'New comment posted',
-      text: 'Hello world!',
-      html: 'Hello world!',
+      subject: 'New Comment Posted on richardwillis.info',
+      text: `A new comment was posted on richardwillis.info.\n\nPost Title: ${article.title}.\n\nApprove or delete the comment: https://strapi.docker-box.richardwillis.info/admin/plugins/content-manager/collectionType/application::comment.comment/${entity.id}`,
+      html: `A new comment was posted on richardwillis.info.<br/><br/>Post Title: ${article.title}.<br/><br/>Approve or delete the comment: <a href="https://strapi.docker-box.richardwillis.info/admin/plugins/content-manager/collectionType/application::comment.comment/${entity.id}">https://strapi.docker-box.richardwillis.info/admin/plugins/content-manager/collectionType/application::comment.comment/${entity.id}</a>`,
     });
     return sanitizeEntity(entity, { model: strapi.models.comment });
   },
